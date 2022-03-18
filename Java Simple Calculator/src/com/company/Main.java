2e6d4d3ef24669aadaaf6e5bbca357e8f86d2aa6
kumarpriyanshu2k2 <priyanshukumar@PRIYANSHUs-MacBook-Air.local>
2022-03-18 23:39:44 +0530

    Java Simple Calculator

---
+++ Java Simple Calculator/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Enter X to exit");
+            System.out.println("Enter operator: ");
+
+            char operator = in.next().trim().charAt(0);
+
+
+            if(operator== '*'|| operator== '+'|| operator== '-'|| operator== '/'){
+                System.out.println("Enter two numbers: ");
+                double n1 = in.nextDouble();
+                double n2 = in.nextDouble();
+                double r=0;
+                if( operator=='*'){
+                    r= n1*n2;
+
+                }
+                if( operator=='+'){
+                    r= n1+n2;
+
+                }
+                if( operator=='-'){
+                    r= n1-n2;
+
+                }
+                if( operator=='/'){
+                    r= n1/n2;
+
+                }
+                System.out.println(r+"");
+            }
+            else if(operator=='x'||operator=='X'){
+                break;
+            }
+            else {
+                System.out.println("Enter a valid operator");
+            }
+        }
+
+
+
+    }
+}
